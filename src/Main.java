a7729752a7747e067e25a46e3352f681a55b0bec
Xavierou <i.dont.know.what.to.write122@gmail.com>
2023-03-19 14:39:05 +0300

    Первая версия задачи "Калькулятор" для Kata Academy

---
+++ src/Main.java
@@ -0,0 +1,101 @@
+import com.sun.jdi.InvalidTypeException;
+
+import java.util.Scanner;
+public class Main {
+    static final Scanner in = new Scanner(System.in);
+    // Объявление массивов tens и ones для облегчения перевода из арабской записи
+    // в римскую
+    static final String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    static final String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    static String to_roman(int num) {
+        return tens[num / 10] + ones[num % 10];
+    }
+
+    /**
+     * Рекурсивная функция для перевода из римской записи в арабскую
+     * @param num - римское число
+     * @return переведенное в арабскую запись число
+     */
+    static int to_arabic(String num) {
+        if (num.isEmpty()) return 0;
+        if (num.startsWith("XC")) return 90 + to_arabic(num.replace("XC", ""));
+        if (num.startsWith("L")) return 50 + to_arabic(num.replace("L", ""));
+        if (num.startsWith("XL")) return 40 + to_arabic(num.replace("XL", ""));
+        if (num.startsWith("X")) return 10 + to_arabic(num.replaceFirst("X", ""));
+        if (num.startsWith("IX")) return 9 + to_arabic(num.replace("IX", ""));
+        if (num.startsWith("V")) return 5 + to_arabic(num.replace("V", ""));
+        if (num.startsWith("IV")) return 4 + to_arabic(num.replace("IV", ""));
+        if (num.startsWith("I")) return 1 + to_arabic(num.replaceFirst("I", ""));
+        return 0;
+    }
+
+    /**
+     * Функция для проверки вводимой строки с применением регулярных выражений
+     * @param expr - вводимая строка
+     * @throws InvalidTypeException - в случае, если строка не соответствует нужному формату
+     */
+    static void parse_expr(String expr) throws InvalidTypeException {
+        String romanStr = "([iIvVxX])+\\s?[-+*/]\\s?([iIvVxX])+";
+        String arabStr = "^\\d?\\s?[-+/*]\\s?\\d?";
+        if (expr.matches(romanStr)) {
+            compute(expr, false);
+        }
+        else if (expr.matches(arabStr)) {
+            compute(expr, true);
+        }
+        else throw new InvalidTypeException("Cannot match pattern");
+    }
+
+    /**
+     * Функция для выполнения арифметических действий
+     * @param str - введенное выражение
+     * @param isArabic - флаг, который говорит, в какой записи данное выражение
+     * @throws ArithmeticException - в случае, если в ходе работы с римскими числами выходит
+     * число <= 0
+     */
+    static void compute(String str, boolean isArabic) throws ArithmeticException {
+        String[] expr = str.split(" ");
+        int result = 0;
+        int num1;
+        int num2;
+        if (isArabic) {
+            num1 = Integer.parseInt(expr[0]);
+            num2 = Integer.parseInt(expr[2]);
+        }
+        else {
+            num1 = to_arabic(expr[0].toUpperCase());
+            num2 = to_arabic(expr[2].toUpperCase());
+        }
+
+        switch (expr[1]) {
+            case "+" -> result = num1 + num2;
+            case "-" -> result = num1 - num2;
+            case "/" -> result = num1 / num2;
+            case "*" -> result = num1 * num2;
+        }
+
+        if (isArabic) {
+            System.out.println(result);
+        }
+        else {
+            // Так как в римской записи число находится в рамках от 1 до 3999
+            // (в контексте задачи - от 1 до 100), нужно проверить,
+            // не вышел ли итоговый результат за эти рамки
+            if ((result >= 1) && (result < 100)) {
+                System.out.println(to_roman(result));
+            }
+            else throw new ArithmeticException("Roman number out of range");
+        }
+    }
+    public static void main(String[] args) {
+        String expression = in.nextLine();
+        try {
+            parse_expr(expression);
+        }
+        catch (InvalidTypeException | ArithmeticException ex) {
+            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Unexpected error");
+        }
+    }
+}
